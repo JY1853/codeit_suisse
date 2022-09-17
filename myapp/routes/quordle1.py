ee466ecbe1b63853295f470c0f43a31a63b7d676
Cheng Jun Yuan <supergambit1@gmail.com>
2022-09-18 00:13:18 +0800

    Update quordle1.py
    added quordle2, changed the functions to return statements instead of printing

--- myapp/routes/quordle1.py
+++ myapp/routes/quordle1.py
@@ -1,5 +1,6 @@
-test_answers = ["VVIDH", "MZLPS", "BPCYN", "XYGGM"]
-test_attempts = ["JKGJB", "ZGRUJ", "XYGGM", "BPCYN", "MHXGE", "DZENT", "ZXWQW", "VVIDH", "MZLPS"]
+# test_answers = ["VVIDH", "MZLPS", "BPCYN", "XYGGM"]
+# test_attempts = ["JKGJB", "ZGRUJ", "XYGGM", "BPCYN", "MHXGE", "DZENT", "ZXWQW", "VVIDH", "MZLPS"]
+# test_numbers = [761, 720, 13, 750, 936, 237, 482, 609, 585, 706, 240, 23, 76, 61, 700, 711, 823, 406, 376, 455, 818, 482, 338, 572, 257]
 
 def letters_in_list(list_of_strings):
     # takes list of strings
@@ -13,13 +14,11 @@ def letters_in_list(list_of_strings):
 def quordle(ans_list, attempt_list):
     answers = ans_list
     letters_in_ans = sorted(letters_in_list(answers))
-    print(letters_in_ans)
     all_letters_guessed = sorted(letters_in_list(attempt_list))
     answer_dict = dict.fromkeys(all_letters_guessed, 0)
 
     for guess in attempt_list:
         unique_chars = set([guess[chr] for chr in range(0, len(guess))])
-        print(unique_chars)
         if guess in answers:
             answers.remove(guess)
             letters_in_ans = sorted(letters_in_list(answers))
@@ -43,4 +42,37 @@ def quordle(ans_list, attempt_list):
     answer_string = ''.join(str(item) for item in answer_dict.values())
     print(answer_string)
 
-quordle(test_answers, test_attempts)
+    return answer_string
+
+def quordle2(ans_list, attempt_list, number_list):
+    full_alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    guessed_letters = sorted(letters_in_list(attempt_list))
+    quordle1_ans = quordle(ans_list, attempt_list)
+    true_false_list = []
+    for num in number_list:
+        if str(num) in quordle1_ans:
+            true_false_list.append("1")
+        else:
+            true_false_list.append("0")
+    split_num_list = [true_false_list[0:5], true_false_list[5:10], true_false_list[10:15], true_false_list[15:20], true_false_list[20:25]]
+    # turn an mf into binary
+    
+    letters = []
+    for i in split_num_list:
+        bin_value = int(''.join(i), 2)
+        letters.append(chr(ord('@')+bin_value))
+    # turn an mf into a letter
+
+    remaining_letters = full_alphabet
+    print(guessed_letters)
+    for i in guessed_letters:
+        if i in remaining_letters:
+            remaining_letters.remove(i)
+    # collect the unused letters
+    
+    for i in remaining_letters:
+        letters.append(i)
+    final_string = ''.join(i for i in letters)
+    # put it together
+
+    return final_string
